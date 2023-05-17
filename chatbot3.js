9e2d6ba7b61dafe9e0d62994cd3c58ae776d542f
alerougon <32884103+alerougon@users.noreply.github.com>
2023-05-17 12:16:29 -0600

    Add files via upload

--- chatbot3.js
+++ chatbot3.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    const API_KEY = 'YOUR_API_KEY'; // Replace with your actual API key
+    const API_KEY = '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'; // Replace with your actual API key
     const chatbotEndpoint = 'https://api.openai.com/v1/chat/completions';
 
     function sendMessage(message) {
