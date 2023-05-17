f7fc5efc645d05389632f2549d49cf1708e5159f
alerougon <32884103+alerougon@users.noreply.github.com>
2023-05-17 12:21:36 -0600

    Add files via upload

---
+++ chatbot4.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    const API_KEY = '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'; // Replace with your actual API key
+    const chatbotEndpoint = 'https://api.openai.com/v1/chat/completions';
+
+    function sendMessage(message) {
+        $.ajax({
+            url: chatbotEndpoint,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + API_KEY
+            },
+            method: 'POST',
+            data: JSON.stringify({
+                'messages': [{'role': 'system', 'content': 'You are a user'}, {'role': 'user', 'content': message}]
+            }),
+            success: function(data) {
+                var response = data.choices[0].message.content;
+                displayMessage(response, 'bot');
+            },
+            error: function(err) {
+                console.error('Error:', err);
+            }
+        });
+    }
+
+    function displayMessage(message, role) {
+        var messageContainer = $('#messages');
+        var messageElement = $('<div>').addClass('message').addClass(role).text(message);
+        messageContainer.append(messageElement);
+    }
+
+    $('#send-button').click(function() {
+        var userMessage = $('#user-input').val();
+        displayMessage(userMessage, 'user');
+        sendMessage(userMessage);
+        $('#user-input').val('');
+    });
+});
